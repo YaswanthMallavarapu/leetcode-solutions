7ceacc0baaa1645916e15bfb16dc5fb81d895964
YaswanthMallavarapu <mallavarapuyaswanth5@gmail.com>
2024-08-02 16:50:04 +0530

    new solution added

---
+++ twoSum-leetcode.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+      
+       HashMap<Integer,Integer>compliments=new HashMap<>();
+        int res[]=new int[2];
+        for(int i=0;i<nums.length;i++){
+            
+            int compliment=target-nums[i];
+            
+            if(compliments.containsKey(compliment)){
+               res[0]=i;
+                res[1]=compliments.get(compliment);
+                break;
+            }
+            compliments.put(nums[i],i);
+        }
+        return res;
+        
+    }
+}
