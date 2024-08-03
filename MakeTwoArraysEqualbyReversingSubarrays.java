a15da739b36617a5beaad295f17d6c0f57c67338
YaswanthMallavarapu <mallavarapuyaswanth5@gmail.com>
2024-08-03 17:28:02 +0530

    new solution added

---
+++ MakeTwoArraysEqualbyReversingSubarrays.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class MakeTwoArraysEqualbyReversingSubarrays {
+    public static void main(String[] args) {
+        //code
+    }
+    
+    public boolean canBeEqual(int[] target, int[] arr) {
+        Arrays.sort(target);
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=target[i])
+                return false;
+        }
+        return true;
+    }
+    
+}
