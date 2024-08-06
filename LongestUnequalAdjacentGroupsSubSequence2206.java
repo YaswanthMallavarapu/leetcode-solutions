8f7c7cf8dd1437bc38d940a9cfac7e11634f3737
YaswanthMallavarapu <mallavarapuyaswanth5@gmail.com>
2024-08-06 16:59:28 +0530

    leetcode2206 added

---
+++ LongestUnequalAdjacentGroupsSubSequence2206.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LongestUnequalAdjacentGroupsSubSequence2206 {
+    
+
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+        List<String>result=new ArrayList<>();
+        int n=words.length;
+      if(words.length==1){
+        result.add(words[0]);
+        return result;
+      }  
+      int prev=groups[0];
+      result.add(words[0]);
+      for(int i=1;i<n;i++){
+        if(groups[i]!=prev){
+            result.add(words[i]);
+            prev=groups[i];
+        }
+      }
+      return result;
+    }
+}
+
+
+
+    
+
